6fe7a909cdd4383e9ea58f97368c55a662b9a576
Patrizio Bertozzi <patrizio.bertozzi@studio.unibo.it>
2024-11-25 11:49:07 +0100

    feat(101-lambda-utilities-for-lists-and-maps): implements lamba utilities

--- 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
+++ 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
@@ -1,7 +1,9 @@
 package it.unibo.oop.lab.lambda;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -14,12 +16,16 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
- * This class will contain four utility functions on lists and maps, of which the first one is provided as example.
+ * This class will contain four utility functions on lists and maps, of which
+ * the first one is provided as example.
  * 
- * All such methods take as second argument a functional interface from the Java library (java.util.function).
- * This enables calling them by using the concise lambda syntax, as it's done in the main function.
+ * All such methods take as second argument a functional interface from the Java
+ * library (java.util.function).
+ * This enables calling them by using the concise lambda syntax, as it's done in
+ * the main function.
  *
- * Realize the three methods **WITHOUT** using the Stream library, but only leveraging the lambdas.
+ * Realize the three methods **WITHOUT** using the Stream library, but only
+ * leveraging the lambdas.
  *
  */
 public final class LambdaUtilities {
@@ -29,11 +35,11 @@ public final class LambdaUtilities {
 
     /**
      * @param list
-     *            the input list
+     *             the input list
      * @param op
-     *            the process to run on each element
+     *             the process to run on each element
      * @param <T>
-     *            element type
+     *             element type
      * @return a new list containing, for each element of list, the element and
      *         a processed version
      */
@@ -48,11 +54,11 @@ public final class LambdaUtilities {
 
     /**
      * @param list
-     *            input list
+     *             input list
      * @param pre
-     *            predicate to execute
+     *             predicate to execute
      * @param <T>
-     *            element type
+     *             element type
      * @return a list where each value is an Optional, holding the previous
      *         value only if the predicate passes, and an Empty optional
      *         otherwise.
@@ -61,18 +67,23 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Optional.filter
          */
-        return null;
+        final List<Optional<T>> items = new ArrayList<>();
+        list.forEach(item -> {
+            final Optional<T> filteredOptional = Optional.of(item).filter(pre);
+            items.add(filteredOptional);
+        });
+        return items;
     }
 
     /**
      * @param list
-     *            input list
+     *             input list
      * @param op
-     *            a function that, for each element, computes a key
+     *             a function that, for each element, computes a key
      * @param <T>
-     *            element type
+     *             element type
      * @param <R>
-     *            key type
+     *             key type
      * @return a map that groups into categories each element of the input list,
      *         based on the mapping done by the function
      */
@@ -80,7 +91,15 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Map.merge
          */
-        return null;
+        final Map<R, Set<T>> map = new HashMap<>();
+        list.forEach(item -> {
+            final R key = op.apply(item);
+            map.merge(key, new HashSet<>(Arrays.asList(item)), (t1, t2) -> {
+                t1.addAll(t2);
+                return t1;
+            });
+        });
+        return map;
     }
 
     /**
@@ -101,16 +120,20 @@ public final class LambdaUtilities {
          *
          * Keep in mind that a map can be iterated through its forEach method
          */
-        return null;
+        final Map<K, V> outMap = new HashMap<>();
+        map.forEach((key, value) -> {
+            outMap.put(key, value.orElse(def.get()));
+        });
+        return outMap;
     }
 
     /**
      * @param args
-     *            ignored
+     *             ignored
      */
     @SuppressWarnings("PMD.SystemPrintln")
     public static void main(final String[] args) {
-        final List<Integer> li = IntStream.range(1, 8).mapToObj(i -> Integer.valueOf(i)).collect(Collectors.toList());
+        final List<Integer> li = IntStream.range(1, 8).mapToObj(Integer::valueOf).collect(Collectors.toList());
         System.out.println(dup(li, x -> x + 100));
         /*
          * [1, 101, 2, 102, 3, 103, 4, 104, 5, 105, 6, 106, 7, 107]
--- 101-lambda-utilities-for-lists-and-maps/src/test/java/it/unibo/oop/lab/lambda/TestLambdaUtilities.java
+++ 101-lambda-utilities-for-lists-and-maps/src/test/java/it/unibo/oop/lab/lambda/TestLambdaUtilities.java
@@ -25,13 +25,11 @@ final class TestLambdaUtilities {
     @Test
     void testDup() {
         assertEquals(
-            List.of(1, 101, 2, 102, 3, 103, 4, 104, 5, 105),
-            LambdaUtilities.dup(List.of(1, 2, 3, 4, 5), x -> x + 100) 
-        );
+                List.of(1, 101, 2, 102, 3, 103, 4, 104, 5, 105),
+                LambdaUtilities.dup(List.of(1, 2, 3, 4, 5), x -> x + 100));
         assertEquals(
-            List.of("a", "aa", "b", "bb", "c", "cc"),
-            LambdaUtilities.dup(List.of("a", "b", "c"), x -> x + x)
-        );
+                List.of("a", "aa", "b", "bb", "c", "cc"),
+                LambdaUtilities.dup(List.of("a", "b", "c"), x -> x + x));
     }
 
     /**
@@ -40,13 +38,12 @@ final class TestLambdaUtilities {
     @Test
     void testOptFilter() {
         assertEquals(
-            List.of(Optional.empty(), Optional.of(2), Optional.empty(), Optional.of(4), Optional.empty(), Optional.of(6)),
-            LambdaUtilities.optFilter(List.of(1, 2, 3, 4, 5, 6), x -> x % 2 == 0)
-        );
+                List.of(Optional.empty(), Optional.of(2), Optional.empty(), Optional.of(4), Optional.empty(),
+                        Optional.of(6)),
+                LambdaUtilities.optFilter(List.of(1, 2, 3, 4, 5, 6), x -> x % 2 == 0));
         assertEquals(
-            List.of(Optional.empty(), Optional.of("bcd"), Optional.of("qw"), Optional.empty(), Optional.empty()),
-            LambdaUtilities.optFilter(List.of("a", "bcd", "qw", "e", ""), x -> x.length() > 1)
-        );
+                List.of(Optional.empty(), Optional.of("bcd"), Optional.of("qw"), Optional.empty(), Optional.empty()),
+                LambdaUtilities.optFilter(List.of("a", "bcd", "qw", "e", ""), x -> x.length() > 1));
     }
 
     /**
@@ -55,20 +52,16 @@ final class TestLambdaUtilities {
     @Test
     void testGroup() {
         assertEquals(
-            Map.of(
-                "even", Set.of(2, 4),
-                "odd", Set.of(1, 3, 5)
-            ),
-            LambdaUtilities.group(List.of(1, 2, 3, 4, 5), x -> x % 2 == 0 ? "even" : "odd")
-        );
+                Map.of(
+                        "even", Set.of(2, 4),
+                        "odd", Set.of(1, 3, 5)),
+                LambdaUtilities.group(List.of(1, 2, 3, 4, 5), x -> x % 2 == 0 ? "even" : "odd"));
         assertEquals(
-            Map.of(
-                3, Set.of("abc", "qwe"),
-                2, Set.of("zx", "cv"),
-                1, Set.of("o")
-            ),
-            LambdaUtilities.group(List.of("abc", "qwe", "zx", "o", "cv"), x -> x.length())
-        );
+                Map.of(
+                        3, Set.of("abc", "qwe"),
+                        2, Set.of("zx", "cv"),
+                        1, Set.of("o")),
+                LambdaUtilities.group(List.of("abc", "qwe", "zx", "o", "cv"), String::length));
     }
 
     /**
@@ -78,19 +71,17 @@ final class TestLambdaUtilities {
     void testFill() {
         final var random = new Random();
         final var map = LambdaUtilities.fill(
-            Map.of(
-                "p1", Optional.of(1),
-                "p2", Optional.of(2),
-                "n1", Optional.empty(),
-                "p3", Optional.of(3),
-                "n2", Optional.empty()
-            ),
-            () -> random.nextInt(10) - 10
-        );
+                Map.of(
+                        "p1", Optional.of(1),
+                        "p2", Optional.of(2),
+                        "n1", Optional.empty(),
+                        "p3", Optional.of(3),
+                        "n2", Optional.empty()),
+                () -> random.nextInt(10) - 10);
         assertEquals(map.keySet(), Set.of("p1", "p2", "p3", "n1", "n2"));
-        assertEquals(map.get("p1").intValue(), 1);
-        assertEquals(map.get("p2").intValue(), 2);
-        assertEquals(map.get("p3").intValue(), 3);
+        assertEquals(map.get("p1"), 1);
+        assertEquals(map.get("p2"), 2);
+        assertEquals(map.get("p3"), 3);
         assertTrue(map.get("n1") < 0);
         assertTrue(map.get("n2") < 0);
     }
